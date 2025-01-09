b281d5db7a10373095233edde476fdd7d858bfb0
Brent1986 <brent.lee1986@gmail.com>
2025-01-09 17:10:16 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-2_2FPJKK3FQ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4o0mcumlalrud8686hlh160sqd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2f1j5808opksccrpteelggf6nh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
