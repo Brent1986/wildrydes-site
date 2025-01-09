0af1f6e80af02d0a3a407326e4ea5231790ed1cf
Brent1986 <brent.lee1986@gmail.com>
2025-01-09 17:20:28 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_2FPJKK3FQ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2f1j5808opksccrpteelggf6nh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2' // e.g. us-east-2
+        userPoolId: 'us-east-1_qIcBnosYC', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2ub8d18gvvbalrhoc5e34adog5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
